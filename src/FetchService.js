e6c7f2659fa02aeb9a2b3024372c109a6a885c5e
Chad Autry <chad.autry@gmail.com>
2017-06-15 06:47:13 -0500

    Implement the first method of the fetch service

--- src/FetchService.js
+++ src/FetchService.js
@@ -3,10 +3,27 @@
 module.exports = class FetchService {
     constructor() {
         this.setAuthService = this.setAuthService.bind(this);
+        this.postWithAuth = this.postWithAuth.bind(this);
     }
 
     setAuthService(authService) {
        this.authService = authService; 
     }
 
+    postWithAuth(url, body, andThen, noAndThen) {
+        //Since JS is single threaded, no need to worry the token can change after checking isAuthenticated
+        if (this.authService.isAuthenticated()) {
+            fetch(url, {
+	        method: 'post',
+                headers: new Headers({
+	             'Content-Type': 'text/plain',
+                     'Authorization': 'JWT ' + this.authService.getToken()
+                },
+	        body: new FormData(body))
+            }).then(andThen).error(noAndThen);
+        } else {
+            //TODO Create an error response
+            noAndThen();
+        }
+    }
 };
