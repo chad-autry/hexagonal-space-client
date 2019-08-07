960b239ae0101d6a2c57a361755e807efaff1ee0
Chad Autry <chad.autry@gmail.com>
2019-08-06 19:58:08 -0500

    Begin to work on docs

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -2,7 +2,32 @@ import React from "react";
 
 const Docs = class Docs extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Intro</h3>
+          </div>
+          <div className="panel-body">
+            hexagonal.space is a persistent shared world sandbox programming
+            game. In it you and others submit scripts which control your fleet
+            of ships to explore the galaxy. Each game turn is 15 minutes long
+          </div>
+        </div>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Turn Sequence</h3>
+          </div>
+          <div className="panel-body" />
+        </div>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Ship Components</h3>
+          </div>
+          <div className="panel-body" />
+        </div>
+      </div>
+    );
   }
 };
 
