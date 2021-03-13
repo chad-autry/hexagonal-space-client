7e45d14cda99771c86497f1814e288de6a489b19
Chad Autry <>
2021-03-13 16:38:22 +0000

    Add SVG display. Move view type selection to navbar

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,6 +13,7 @@ import DataSource from "hex-grid-map/src/dataSources/DataSource.js";
 import CellDrawnItemFactory from "hex-grid-map/src/drawnItemFactories/RegularPolygonDrawnItemFactory";
 import SphereDrawnItemFactory from "hex-grid-map/src/drawnItemFactories/SphereDrawnItemFactory";
 import FieldOfSquaresDrawnItemFactory from "hex-grid-map/src/drawnItemFactories/FieldOfSquaresDrawnItemFactory";
+import SVGDrawnItemFactory from "hex-grid-map/src/drawnItemFactories/SVGDrawnItemFactory";
 import HexDefinition from "cartesian-hexagonal";
 import makeDataLink from "data-chains/src/DataLinkMixin";
 import EmittingDataSource from "data-chains/src/EmittingDataSource.js";
@@ -30,6 +31,198 @@ const Map = class Map extends React.Component {
     // This line is important!
     this.setComponentState = this.setComponentState.bind(this);
     this.baseDataLink = props.dataLink;
+    let rocketSVG = `<svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 640 512"><!-- Font Awesome Pro 5.15.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M592.604 208.244C559.735 192.836 515.777 184 472 184H186.327c-4.952-6.555-10.585-11.978-16.72-16H376C229.157 137.747 219.403 32 96.003 32H96v128H80V32c-26.51 0-48 28.654-48 64v64c-23.197 0-32 10.032-32 24v40c0 13.983 8.819 24 32 24v16c-23.197 0-32 10.032-32 24v40c0 13.983 8.819 24 32 24v64c0 35.346 21.49 64 48 64V352h16v128h.003c123.4 0 133.154-105.747 279.997-136H169.606c6.135-4.022 11.768-9.445 16.72-16H472c43.777 0 87.735-8.836 120.604-24.244C622.282 289.845 640 271.992 640 256s-17.718-33.845-47.396-47.756zM488 296a8 8 0 0 1-8-8v-64a8 8 0 0 1 8-8c31.909 0 31.942 80 0 80z"/></svg>`;
+    //Eventually have a chain with filters, decorators, and such
+    this.baseDataLink.addListener({
+      onDataChanged: event => {
+        //TODO clear/update cellDataSource
+        //TODO refactor item transformation, the Map's constructor is not the best place
+        this.baseDataLink.forEach(element => {
+          let rotation = 0;
+          let shipDirectionVector = {};
+          switch (element.subType) {
+            case "star":
+              this.cellDataSource.addItems([
+                {
+                  type: "sphere",
+                  size: 100,
+                  rotation: 63.435 * (Math.PI / 180),
+                  lineWidth: 3,
+                  greatCircleAngles: [-Math.PI / 6, Math.PI / 6, Math.PI / 2],
+                  latitudeAngles: [0, Math.PI / 6, Math.PI / 3, -Math.PI / 6],
+                  lineColor: "orange",
+                  backgroundColor: "yellow",
+                  borderStar: {
+                    radius1: 3,
+                    radius2: 6,
+                    points: 20,
+                    borderColor: "orange"
+                  },
+                  u: element.states.final.u,
+                  v: element.states.final.v
+                }
+              ]);
+              break;
+            case "planet":
+              this.cellDataSource.addItems([
+                {
+                  type: "sphere",
+                  size: 66,
+                  rotation: 63.435 * (Math.PI / 180),
+                  lineWidth: 3,
+                  greatCircleAngles: [-Math.PI / 6, Math.PI / 6, Math.PI / 2],
+                  latitudeAngles: [0, Math.PI / 6, Math.PI / 3, -Math.PI / 6],
+                  lineColor: "#653700",
+                  backgroundColor: "blue",
+                  borderWidth: 2,
+                  borderColor: "white",
+                  u: element.states.final.u,
+                  v: element.states.final.v
+                }
+              ]);
+              break;
+            case "moon":
+              //Add a sphere to represent the moon
+              this.cellDataSource.addItems([
+                {
+                  type: "sphere",
+                  size: 33,
+                  rotation: 63.435 * (Math.PI / 180),
+                  lineWidth: 2,
+                  greatCircleAngles: [-Math.PI / 6, Math.PI / 6, Math.PI / 2],
+                  latitudeAngles: [0, Math.PI / 6, Math.PI / 3, -Math.PI / 6],
+                  lineColor: "grey",
+                  backgroundColor: "#E1E1D6",
+                  borderWidth: 3,
+                  borderColor: "black",
+                  u: element.states.final.u,
+                  v: element.states.final.v
+                }
+              ]);
+              break;
+            case "gravity":
+              //Add arrow to represent gravity
+
+              if (
+                element.states.final.uGrav == 0 &&
+                element.states.final.vGrav == 1
+              ) {
+                rotation = 180;
+              } else if (
+                element.states.final.uGrav == 1 &&
+                element.states.final.vGrav == 0
+              ) {
+                rotation = 240;
+              } else if (
+                element.states.final.uGrav == 1 &&
+                element.states.final.vGrav == -1
+              ) {
+                rotation = 300;
+              } else if (
+                element.states.final.uGrav == 0 &&
+                element.states.final.vGrav == -1
+              ) {
+                rotation = 0;
+              } else if (
+                element.states.final.uGrav == -1 &&
+                element.states.final.vGrav == 0
+              ) {
+                rotation = 60;
+              } else if (
+                element.states.final.uGrav == -1 &&
+                element.states.final.vGrav == 1
+              ) {
+                rotation = 120;
+              }
+              this.cellDataSource.addItems([
+                {
+                  type: "arrow",
+                  u: element.states.final.u,
+                  v: element.states.final.v,
+                  fillColor: "grey",
+                  lineWidth: 0,
+                  lineColor: "grey",
+                  rotation: rotation,
+                  scaleLength: 0.75,
+                  scaleWidth: 0.75
+                }
+              ]);
+              break;
+            case "halfGrav":
+              //Add arrow to represent halfGrav
+              if (
+                element.states.final.uGrav == 0 &&
+                element.states.final.vGrav == 1
+              ) {
+                rotation = 180;
+              } else if (
+                element.states.final.uGrav == 1 &&
+                element.states.final.vGrav == 0
+              ) {
+                rotation = 240;
+              } else if (
+                element.states.final.uGrav == 1 &&
+                element.states.final.vGrav == -1
+              ) {
+                rotation = 300;
+              } else if (
+                element.states.final.uGrav == 0 &&
+                element.states.final.vGrav == -1
+              ) {
+                rotation = 0;
+              } else if (
+                element.states.final.uGrav == -1 &&
+                element.states.final.vGrav == 0
+              ) {
+                rotation = 60;
+              } else if (
+                element.states.final.uGrav == -1 &&
+                element.states.final.vGrav == 1
+              ) {
+                rotation = 120;
+              }
+              this.cellDataSource.addItems([
+                {
+                  type: "arrow",
+                  u: element.states.final.u,
+                  v: element.states.final.v,
+                  fillColor: "black",
+                  lineWidth: 3,
+                  lineColor: "grey",
+                  rotation: rotation,
+                  scaleLength: 0.75,
+                  scaleWidth: 0.75
+                }
+              ]);
+              break;
+            case "ship":
+              shipDirectionVector = this.hexDimensions.getPixelCoordinates(
+                element.states.final.uVel,
+                element.states.final.vVel
+              );
+              rotation =
+                (-1 *
+                  180 *
+                  Math.atan2(
+                    -1 * shipDirectionVector.y,
+                    shipDirectionVector.x
+                  )) /
+                Math.PI;
+              this.cellDataSource.addItems([
+                {
+                  type: "svg",
+                  scale: 0.5,
+                  svg: rocketSVG,
+                  u: element.states.final.u,
+                  v: element.states.final.v,
+                  rotation: rotation
+                }
+              ]);
+          }
+        });
+      }
+    });
+    //TODO remove listener in componentDidUnmount
   }
 
   render() {
@@ -80,20 +273,89 @@ const Map = class Map extends React.Component {
     };
     this.resizeListener = resizeFunction;
     window.addEventListener("resize", this.resizeListener);
-    //babylon.js is controlling the size, force it to resize using our container size when opened
-    //            this.props.glContainer.on('open', resizeFunction);
-
-    //babylon.js is controlling the size, force it to resize using our container size on golden-layout resize
-    //           this.props.glContainer.on('resize', resizeFunction);
 
     this.hexBoard = new HexBoard(this.canvasRef);
     //TODO Temp hard coded hexDimensions
     let hexDimensions = new HexDefinition(55, 0.5, 0, 3);
+    this.hexDimensions = hexDimensions;
     this.hexBoard.setHexDimensions(hexDimensions);
-    this.hexBoard.setContexts([
-      new GridContext(hexDimensions),
-      new BackgroundContext()
-    ]);
+
+    let contexts = [];
+    contexts.push(new BackgroundContext());
+    //Create the cell items datasource, drawnItemFactories, and special compound contex
+    let cellDataSource = new DataSource();
+    this.cellDataSource = cellDataSource;
+    let simpleDrawnItemFactory = new CellDrawnItemFactory(hexDimensions);
+    let sphereDrawnItemFactor = new SphereDrawnItemFactory(hexDimensions);
+    let arrowDrawnItemFactory = new ArrowDrawnItemFactory(hexDimensions);
+    let svgDrawnItemFactory = new SVGDrawnItemFactory(hexDimensions);
+
+    //For Asteroids we use brown grey, brownish grey, greyish brown, grey brown. For debris would probablly go more blue-grey
+    let asteroidFieldDrawnItemFactory = new FieldOfSquaresDrawnItemFactory(
+      hexDimensions,
+      9,
+      20,
+      ["#8d8468", "#86775f", "#7a6a4f", "#7f7053"]
+    );
+    let cellDrawnItemFactoryMap = {
+      simple: simpleDrawnItemFactory,
+      sphere: sphereDrawnItemFactor,
+      arrow: arrowDrawnItemFactory,
+      asteroids: asteroidFieldDrawnItemFactory,
+      svg: svgDrawnItemFactory
+    };
+    let cellDrawnItemFactory = new DelegatingDrawnItemFactory(
+      cellDrawnItemFactoryMap
+    );
+    let cellContext = new CellContext(
+      cellDataSource,
+      cellDrawnItemFactory,
+      5,
+      hexDimensions
+    );
+
+    //Push the below grid portion of the cell context
+    contexts.push(cellContext.belowGridContext);
+
+    //Create and push the grid context
+    contexts.push(new GridContext(hexDimensions));
+
+    //Define and push the paths DataSource, DrawnItemFactory, and Context
+    let pathDataSource = new DataSource();
+    let pathDrawnItemFactory = new PathDrawnItemFactory(hexDimensions);
+    contexts.push(
+      new DrawnItemContext(pathDataSource, pathDrawnItemFactory, hexDimensions)
+    );
+
+    //Definte and push the vector DataSource, DrawnItemFactory, and Context
+    let vectorDataSource = new DataSource();
+    let vectorDrawnItemFactory = new VectorDrawnItemFactory(hexDimensions);
+    contexts.push(
+      new DrawnItemContext(
+        vectorDataSource,
+        vectorDrawnItemFactory,
+        hexDimensions
+      )
+    );
+
+    //Push the above grid cell context defined earlier
+    contexts.push(cellContext);
+
+    //Create and push the LensFlareContext
+    contexts.push(new ForegroundContext([{ u: 0, v: 0 }], hexDimensions));
+    let globalMouseClicked = (dx, x, dy, y) => {
+      var hexagonalCoordinates = hexDimensions.getReferencePoint(
+        x - dx,
+        y - dy
+      );
+      this.props.addAlert({
+        type: "info",
+        text:
+          "Clicked U:" + hexagonalCoordinates.u + " V:" + hexagonalCoordinates.v
+      });
+    };
+    this.hexBoard.setMouseClicked(globalMouseClicked);
+    this.hexBoard.setContexts(contexts);
 
     this.hexBoard.init();
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,9 +9,10 @@ import NavItem from "./NavItem.jsx";
 const NavBar = class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { menuCollapsed: true };
+    this.state = { menuCollapsed: true, viewDropdownCollapsed: true };
     // This line is important!
     this.menuClicked = this.menuClicked.bind(this);
+    this.viewDropDownClicked = this.viewDropDownClicked.bind(this);
   }
 
   menuClicked() {
@@ -20,18 +21,35 @@ const NavBar = class NavBar extends React.Component {
     });
   }
 
+  viewDropDownClicked() {
+    this.setState({
+      viewDropdownCollapsed: !this.state.viewDropdownCollapsed
+    });
+  }
+
   shouldComponentUpdate(nextProps, nextState) {
     // Don't blow the stack out by re-rendering when this components height is set to the parent
     return (
       this.props.isAuthenticated != nextProps.isAuthenticated ||
       this.state.menuCollapsed != nextState.menuCollapsed ||
+      this.state.viewDropdownCollapsed != nextState.viewDropdownCollapsed ||
       this.props.location.pathname != nextProps.location.pathname
     );
   }
 
+  componentDidUpdate(nextProps) {
+    //collapse the dropdown if an element on it was selected
+    if (
+      this.props.location.pathname != nextProps.location.pathname &&
+      !this.state.viewDropdownCollapsed
+    ) {
+      this.setState({ viewDropdownCollapsed: true });
+    }
+  }
+
   render() {
     return (
-      <div className="navbar navbar-default" style={{ zIndex: 300 }}>
+      <div className="navbar navbar-default" style={{ zIndex: 301 }}>
         <div className="navbar-header" onClick={this.menuClicked}>
           <div className="navbar-toggle">
             <span className="sr-only">Toggle navigation</span>
@@ -56,9 +74,52 @@ const NavBar = class NavBar extends React.Component {
               : "navbar-collapse"
           }>
           <ul className="nav navbar-nav">
-            <NavItem to="/view" location={this.props.location}>
-              <i className="fa fa-eye" /> View
-            </NavItem>
+            <li
+              className={
+                this.props.location.pathname.startsWith("/view")
+                  ? "dropdown active"
+                  : "dropdown"
+              }>
+              <a
+                href="#"
+                className="dropdown-toggle"
+                onClick={this.viewDropDownClicked}
+                role="button"
+                aria-haspopup="true">
+                {this.props.location.pathname.startsWith("/view/map") ? (
+                  <div>
+                    {" "}
+                    <i className="fa fa-map" /> Map <span className="caret" />
+                  </div>
+                ) : this.props.location.pathname.startsWith("/view/table") ? (
+                  <div>
+                    {" "}
+                    <i className="fa fa-list" /> List <span className="caret" />
+                  </div>
+                ) : (
+                  <div>
+                    <i className="fa fa-eye" /> View <span className="caret" />
+                  </div>
+                )}
+              </a>
+              <ul
+                className={
+                  this.state.viewDropdownCollapsed
+                    ? "dropdown-menu hidden"
+                    : "dropdown-menu"
+                }>
+                {!this.props.location.pathname.startsWith("/view/map") ? (
+                  <NavItem to="/view/map" location={this.props.location}>
+                    <i className="fa fa-map" /> Map
+                  </NavItem>
+                ) : null}
+                {!this.props.location.pathname.startsWith("/view/table") ? (
+                  <NavItem to="/view/table" location={this.props.location}>
+                    <i className="fa fa-list" /> List
+                  </NavItem>
+                ) : null}
+              </ul>
+            </li>
             {/*We only ships the code NavItem if the user is logged on*/}
             {this.props.isAuthenticated ? (
               <NavItem to="/ships" location={this.props.location}>
